b41c52c4998a5fde648b75880f737d3538ffe98e
YjjTT <yjjtt@tempot.com>
2019-03-22 14:50:50 +0800

    Updates

---
+++ precache-manifest.e8bfd8cb08ab7783a04b8937140c3d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "555a0afcd67e989bd347",
+    "url": "/static/css/main.6de49b68.chunk.css"
+  },
+  {
+    "revision": "555a0afcd67e989bd347",
+    "url": "/static/js/main.0d3acd39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50db364589e498a2aede",
+    "url": "/static/js/2.011a8211.chunk.js"
+  },
+  {
+    "revision": "7bbf83900d51f5a5e814cca703b07cf2",
+    "url": "/index.html"
+  }
+];
