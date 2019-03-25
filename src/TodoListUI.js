ee26c6f582ceff5b19b72996e3f96c05670ac1cc
YjjTT <yjjtt@tempot.com>
2019-03-25 21:43:31 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component{
-    render(){
-        return(
-            <div style={{marginTop: 10, marginLeft: 10}}>
-                <Input value={this.props.inputValue} 
+const TodoListUI = (props) => {
+    return(
+        <div style={{marginTop: 10, marginLeft: 10}}>
+            <Input value={props.inputValue} 
                 placeholder="search" 
                 style={{ width: 300, marginRight: 10}} 
-                onChange={this.props.handleInputChange}
+                onChange={props.handleInputChange}
                 />
-                <Button type="primary" onClick={this.props.handleButtonClick}>提交</Button>
-                <List
-                    style={{marginTop: 10, width: 300}}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (
-                    <List.Item 
-                    onClick={(index) => {this.props.handleItemClick(index)}}>
-                    {item}
-                    </List.Item>
-                    )}
-                />
-            </div>
-        )
-    }
+            <Button type="primary" onClick={props.handleButtonClick}>提交</Button>
+            <List
+                style={{marginTop: 10, width: 300}}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (
+            <List.Item 
+                onClick={(index) => {props.handleItemClick(index)}}>
+                {item}
+            </List.Item>
+            )}
+            />
+        </div>
+    )
 }
+// class TodoListUI extends Component{
+//     render(){
+//         return(
+            
+//         )
+//     }
+// }
 export default TodoListUI;
